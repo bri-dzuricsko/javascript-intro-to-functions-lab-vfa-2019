46376636698e037dc31a3b1f8a39d4bf5e0bcd92
Brianna Dzuricsko <bri.dzuricsko@gmail.com>
2019-06-11 15:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function whisper (string)
 {
   return string.toLowerCase ()
 }
+
+function logShout
+
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
