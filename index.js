07790a4e64a0368e0f9cece4aa170a4f3d3dce80
Brianna Dzuricsko <bri.dzuricsko@gmail.com>
2019-06-11 16:01:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper (string){
 
 function sayHiToGrandma(string){
   if (string === lowercase.toLowerCase) return "I can't hear you!";
-  elseif (string === uppercase.toUpperCase) return "YES INDEED!";
+  else if (string === uppercase.toUpperCase) return "YES INDEED!";
   
 }
 
