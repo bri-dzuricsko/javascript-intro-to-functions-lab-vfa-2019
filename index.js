40ec377337f0fb54e8bfd8f89a52982a285cfe61
Brianna Dzuricsko <bri.dzuricsko@gmail.com>
2019-06-11 15:59:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function logWhisper (string){
 }
 
 function sayHiToGrandma(string){
-  if (string === lowercase.toLowerCase) return
+  if (string === lowercase.toLowerCase) return "I can\'t hear you!"
+  
 }
 
 
